261b4435cf13f11895bd3d465ad68673d20b5cc2
travism1888 <38265370+travism1888@users.noreply.github.com>
2018-10-30 20:22:37 -0700

    Update VoiceStats.py
    Updated the functions to parse and return max using a dict

--- VoiceStats.py
+++ VoiceStats.py
@@ -1,46 +1,26 @@
 def parse_sessions(inputFile):
-    inner = False
-    resultList = list()
-    
-    if not isinstance(inputFile, list): #determines if the variable passed in is already a list
-        splitFile = inputFile.split('\n') #split the string into a list. Separate the string on ever newline
-    else:
-        splitFile = inputFile
-    itemList = list()
+    resultDict = {} #dict is a useful data structure for this case
+    splitFile = inputFile.split('\n') #split the string into a list. Separate the string on every newline
     for line in splitFile:
+        l = line.strip() #using a stripped line normalizes the left side whitespace in order to count the columns accurately
         if 'Capacity=' in line: #locate the begining of a chunk
-            inner = True #set to true to capture other data point
-            splitLine = line.strip().split(' ')
-            itemList.append(splitLine[0])
-        if inner and 'Total Sessions' in line:
-            splitLine = line.split(' ')
-            x = 1
-            for line in splitLine:
-                if line == '':
-                    splitLine.pop(splitLine.index(line))            
-            itemList.append(splitLine[3]) #adds datapoint to inner list
-            inner = False #Set to false to show it's done with that time stamp
-            resultList.append(itemList) #adds innerlist to result list
-            itemList = list()
-    return resultList #list returned is a list of lists containing a timestamp, and that times highest value
-
-def max_session(inputList): #locates the highest value of the list passed in
-    maxSession = list()
-    current = 0
-    max = 0
-    for item in range(len(inputList)):
-        session = inputList[item]
-        current = int(session[1])
-        if current > max:
-            maxSession = inputList[item]
-            max = current
+            key = l[:5] #slice the string to include the first 5 characters for the timestamp hh:mm
+        if 'Total	Sessions' in line:
+            splitLine = l.split('	')
+            value = int(splitLine[4]) #must include the explicit conversion to int or the max function doesnt work right
+            resultDict[key] = value
+    return resultDict #return Dict of Key = Timestamp, Value = Total Sessions
+def max_session(inputDict): #locates the highest value of the list passed in
+    keys = inputDict.keys()
+    values = inputDict.values()
+    maxValue = max(values)
+    maxSession = []
+    for key, value in inputDict.items():
+        if value == maxValue:
+            maxSession.append(key + ': ' + str(value)) #explicitly change the session int back to string for printing
     return maxSession #returns a list of a timestamp and a value
-
-
-if __name__ == '__main__':
-    #Read in a file named SIPCalls.txt
-    with open('SIPCalls.txt', 'r') as f:
-        file = f.read()
-    intermediate = parse_sessions(file)
-    result = max_session(intermediate)
-    print(result)        
+with open('SIPCalls.txt', 'r') as f:
+    file = f.read()
+intermediate = parse_sessions(file)
+result = max_session(intermediate)
+print(result)        
